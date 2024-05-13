7bc1d2c9c6abd772c8fdf5be8b2eba5f94a58d01
juanOchoa44 <140007421+juanOchoa44@users.noreply.github.com>
2024-05-13 15:41:01 -0500

    Responsive 100%

--- Templates_Pruebas/home/scripts/hMenu.js
+++ Templates_Pruebas/home/scripts/hMenu.js
@@ -6,6 +6,20 @@ const option = document.querySelector(".option_drop");
 const options = document.querySelectorAll(".option_drop li");
 const active = document.querySelector(".active_drop");
 
+// If the dropmenu is active and the click is out of this
+window.addEventListener("click", (e) => {
+    if (e.target.className != "select_drop select_drop-clicked"
+    && e.target.className != "selected_drop"
+    && e.target.className != "caret_drop caret_drop-rotate"
+    && select.classList.contains("select_drop-clicked")) {
+        //The menu is off
+        select.classList.remove("select_drop-clicked");
+        caret.classList.remove("caret_drop-rotate");
+        option.classList.remove("option_drop-open");
+    }
+})
+
+// Dropdown menu functions
 select.addEventListener("click", () => {
     select.classList.toggle("select_drop-clicked");
     caret.classList.toggle("caret_drop-rotate");
@@ -13,17 +27,11 @@ select.addEventListener("click", () => {
 });
 
 options.forEach(element => {
-
     element.addEventListener("click", () => {
-        selected.innerText = element.innerText;
         select.classList.remove("select_drop-clicked");
         caret.classList.remove("caret_drop-rotate");
         option.classList.remove("option_drop-open");
-
-        options.forEach(element2 => {
-            element2.classList.remove("active_drop");
-        });
-
-        element.classList.add("active_drop");
     });
 });
+
+//selected_drop caret_drop caret_drop-rotate
