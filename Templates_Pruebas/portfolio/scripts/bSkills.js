330443f4827ce91f1df06abdf4c33532ea69ce00
juanOchoa44 <140007421+juanOchoa44@users.noreply.github.com>
2024-05-27 12:20:57 -0500

    Efecto de hover en habilidades finalizado

--- Templates_Pruebas/portfolio/scripts/bSkills.js
+++ Templates_Pruebas/portfolio/scripts/bSkills.js
@@ -1,14 +1,19 @@
 const skillBlocks = document.querySelectorAll(".main-body-skills .main");
+const middleBlock = document.querySelector(".main-body-skills .data_and_dev");
 
-skillBlocks.forEach(element => {
-    element.addEventListener("mouseenter", () => {
-        var timerBorder1 = setTimeout(() => {
-            element.classList.toggle("border_top_bot_hover");
-        }, 500);
-    })
 
-    element.addEventListener("mouseleave", () => {
-        clearTimeout(timerBorder1);
-        element.classList.remove("border_top_bot_hover");
-    })
-})
+skillBlocks[0].addEventListener("mouseenter", () => {
+    middleBlock.children[0].classList.toggle("skill_left_active");
+});
+
+skillBlocks[0].addEventListener("mouseleave", () => {
+    middleBlock.children[0].classList.remove("skill_left_active");
+});
+
+skillBlocks[1].addEventListener("mouseenter", () => {
+    middleBlock.children[0].classList.toggle("skill_right_active");
+});
+
+skillBlocks[1].addEventListener("mouseleave", () => {
+    middleBlock.children[0].classList.remove("skill_right_active");
+});
