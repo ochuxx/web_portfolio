c5edc59196ad778176bfd76ec8b13dd606413fd9
juanOchoa44 <140007421+juanOchoa44@users.noreply.github.com>
2024-08-22 22:49:07 -0500

    Componente para renderizar habilidades

--- template-react/src/content/02-portfolio/SkillsList.jsx
+++ template-react/src/content/02-portfolio/SkillsList.jsx
@@ -1,5 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChartSimple } from '@fortawesome/free-solid-svg-icons'
+import dataSkills from '@skills-json'
+
+function SkillChart({title, headerIcon, profession}) {
+  return (
+    <div>
+      <header>
+        <FontAwesomeIcon icon={headerIcon} />
+        <h3>{title}</h3>
+      </header>
+      <ul>
+        {
+          dataSkills.map(props => {
+            if (props.profession == profession) {
+              return (
+                <li key={props.id}>
+                  <img src={props.icon} />
+                  <span>{props.name}</span>
+                </li>
+              )
+            }
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
 export function SkillsList() {
   return (
-    <h1>Esta es la sección de las habilidades</h1>
+    <section>
+      <h2>Habilidades</h2>
+      <div>
+        <SkillChart headerIcon={faChartSimple} title='Análisis de datos' profession='data' />
+      </div>
+    </section>
   )
 }
