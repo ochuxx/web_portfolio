fd25859e694035d72dc1f031d086cad485737d4a
Juan Ochoa <ochux.project@gmail.com>
2024-08-26 14:59:51 -0500

    Avance en estilo de skills y github añadido

--- template-react/src/content/02-portfolio/SkillsList.jsx
+++ template-react/src/content/02-portfolio/SkillsList.jsx
@@ -1,39 +1,51 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChartSimple, faCode } from '@fortawesome/free-solid-svg-icons'
+import { faChartColumn, faCode } from '@fortawesome/free-solid-svg-icons'
 import dataSkills from '@skills-json'
+import styles from '@styles/content/02-portfolio/SkillsList.module.css'
 
-function SkillChart({title, headerIcon, profession}) {
+function SkillCards({children}) {
   return (
-    <div>
-      <header>
-        <FontAwesomeIcon icon={headerIcon} />
-        <h3>{title}</h3>
+    <ul className={styles['cards-container']}>
+      {children.map(data => (
+        <li key={data.id} className={styles.card}>
+          <img 
+            src={data.icon}
+            alt='icon'
+            className={styles['card__icon']}
+          />
+          <span className={styles['card__name']}>{data.name}</span>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function SkillsBox({title, headerIcon, dataList, profession = 'data_dev'}) {
+  return (
+    <div className={`${styles.box} ${styles[`box--${profession}`]}`}>
+      <header className={styles['box__header']}>
+        <FontAwesomeIcon icon={headerIcon} className={styles['box__header__icon']} />
+        <h3 className={styles['box__header__title']}>{title}</h3>
       </header>
-      <ul>
-        {
-          dataSkills.map(props => {
-            if (props.profession == profession) {
-              return (
-                <li key={props.id}>
-                  <img src={props.icon} />
-                  <span>{props.name}</span>
-                </li>
-              )
-            }
-          })
-        }
-      </ul>
+      <SkillCards>
+        {dataList}
+      </SkillCards>
     </div>
   )
 }
 
 export function SkillsList() {
+  const dataAnalyst = dataSkills.filter(data => data.profession == 'data')
+  const dataDevelopment = dataSkills.filter(data => data.profession == 'development')
+  const dataBoth = dataSkills.filter(data => data.profession == 'data_developmet')
+
   return (
-    <section>
-      <h2>Habilidades</h2>
-      <div>
-        <SkillChart headerIcon={faChartSimple} title='Análisis de datos' profession='data' />
-        <SkillChart headerIcon={faCode} title='Desarrollo web' profession='development' />
+    <section className={styles['title-box-container']}>
+      <h2 className={styles['skill-title']}>Habilidades</h2>
+      <div className={styles['boxes-container']}>
+        <SkillsBox headerIcon={faChartColumn} title='Análisis de datos' dataList={dataAnalyst} profession='data' />
+        <SkillsBox headerIcon={faCode} title='' dataList={dataBoth} />
+        <SkillsBox headerIcon={faCode} title='Desarrollo web' dataList={dataDevelopment} profession='dev' />
       </div>
     </section>
   )
--- template-react/src/header/SocialMedia.jsx
+++ template-react/src/header/SocialMedia.jsx
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWhatsapp, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faWhatsapp, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import styles from '@styles/header/SocialMedia.module.css'
 
 function SocialMediaIcon({symbol, link, variant}) {
   const variantClass = `icon-link__symbol--${variant}`
 
   return (
-    <a href={link} className={styles['icon-link']}>
+    <a href={link} className={styles['icon-link']} target='_blank'>
       <FontAwesomeIcon
         className={`${styles['icon-link__symbol']} ${styles[variantClass]}`}
         icon={symbol}
@@ -19,7 +19,8 @@ export function SocialMedia() {
   return (
     <section className={styles['social-media']}>
       <SocialMediaIcon symbol={faWhatsapp} link='#' variant='whatsapp' />
-      <SocialMediaIcon symbol={faLinkedin} link='#' variant='linkedin' />
+      <SocialMediaIcon symbol={faLinkedin} link='https://www.linkedin.com/in/ochuxx/' variant='linkedin' />
+      <SocialMediaIcon symbol={faGithub} link='https://github.com/ochuxx' variant='github' />
     </section>
   )
 }
