edf53830f4107f71fd6a591235033b11846e34c4
ochuxx <ochux.project@gmail.com>
2024-11-20 22:41:38 -0500

    Se reemplaza WhatsApp por Instagram en SocialMedia
    Se termina de agregar los componentes a la sección de contacto, falta diseño

--- template-react/src/components/content/04-contact/Contact.jsx
+++ template-react/src/components/content/04-contact/Contact.jsx
@@ -1,5 +1,7 @@
 import { React } from 'react'
 import { ContactForm } from './ContactForm'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import styles from '@styles/content/04-contact/Contact.module.css'
 
 export function Contact() {
@@ -7,10 +9,14 @@ export function Contact() {
     <section className={styles['contact-container']}>
       <div>
         <aside>
-
+          <img src="" alt="" />
+          <div>
+            <span>ochux.project@gmail.com</span>
+          </div>
         </aside>
         <div>
           <ContactForm />
+          <FontAwesomeIcon icon={faEnvelope} />
         </div>
       </div>
     </section>
--- template-react/src/components/header/SocialMedia.jsx
+++ template-react/src/components/header/SocialMedia.jsx
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWhatsapp, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import styles from '@styles/header/SocialMedia.module.css'
 
 function SocialMediaIcon({ symbol, link, variant }) {
   const variantClass = `icon-link__symbol--${variant}`
 
   return (
-    <a href={link} className={styles['icon-link']} target='_blank'>
+    <a href={link} className={`${styles['icon-link']}`} target='_blank'>
       <FontAwesomeIcon
         className={`${styles['icon-link__symbol']} ${styles[variantClass]}`}
         icon={symbol}
@@ -18,7 +18,7 @@ function SocialMediaIcon({ symbol, link, variant }) {
 export function SocialMedia() {
   return (
     <section className={styles['social-media']}>
-      <SocialMediaIcon symbol={faWhatsapp} link='#' variant='whatsapp' />
+      <SocialMediaIcon symbol={faInstagram} link='https://www.instagram.com/oochux/' variant='instagram' />
       <SocialMediaIcon symbol={faLinkedin} link='https://www.linkedin.com/in/ochuxx/' variant='linkedin' />
       <SocialMediaIcon symbol={faGithub} link='https://github.com/ochuxx' variant='github' />
     </section>
