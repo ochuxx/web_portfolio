b8c615991ee79d7dad0b8e6f8a3b563346d008ef
ochuxx <ochux.project@gmail.com>
2024-11-21 23:08:23 -0500

    Diseño de caja para formulario de contacto

--- template-react/src/components/content/04-contact/Contact.jsx
+++ template-react/src/components/content/04-contact/Contact.jsx
@@ -7,16 +7,21 @@ import styles from '@styles/content/04-contact/Contact.module.css'
 export function Contact() {
   return (
     <section className={styles['contact-container']}>
-      <div>
-        <aside>
-          <img src="" alt="" />
-          <div>
-            <span>ochux.project@gmail.com</span>
+      <div className={styles['box']}>
+        <aside className={styles['box__reference']}>
+          <div className={styles['box__reference__background-image']}></div>
+          <div className={styles['box__reference__info']}>
+            <span className={styles['box__reference__info__element']}>
+              ochux.project@gmail.com
+            </span>
           </div>
         </aside>
-        <div>
+        <div className={styles['box__form-container']}>
           <ContactForm />
-          <FontAwesomeIcon icon={faEnvelope} />
+          <FontAwesomeIcon
+            className={styles['box__form-container__mail-icon']}
+            icon={faEnvelope}
+          />
         </div>
       </div>
     </section>
--- template-react/src/components/content/04-contact/ContactForm.jsx
+++ template-react/src/components/content/04-contact/ContactForm.jsx
@@ -1,3 +1,5 @@
+import styles from '@styles/content/04-contact/ContactForm.module.css'
+
 export function ContactForm() {
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -5,25 +7,35 @@ export function ContactForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Contáctame</h2>
-
+    <form
+      className={styles.form}
+      onSubmit={handleSubmit}
+    >
+      <h2 className={styles['form__title']}>Contáctame</h2>
+      
       <input
         type='text'
         name='name'
         placeholder='Nombre / Empresa'
+        className={styles['form__input']}
       />
       <input
         type='text'
         name='email'
         placeholder='Correo'
+        className={styles['form__input']}
       />
       <textarea
         name='message'
         placeholder='Mensaje'
+        className={`${styles['form__input']} ${styles['form__textarea']}`}
       ></textarea>
 
-      <input type='submit' value='Enviar'/>
+      <input
+        type='submit'
+        value='Enviar'
+        className={`${styles['form__input']} ${styles['form__send']}`}
+      />
     </form>
   )
 }
