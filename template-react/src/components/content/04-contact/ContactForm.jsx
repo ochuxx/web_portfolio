b6ea391603fa083c76580890f8d20ddbfb79480a
ochuxx <ochux.project@gmail.com>
2024-11-23 19:23:16 -0500

    Diseño de Contact finalizado

--- template-react/src/components/content/04-contact/ContactForm.jsx
+++ template-react/src/components/content/04-contact/ContactForm.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import styles from '@styles/content/04-contact/ContactForm.module.css'
 
 export function ContactForm() {
+  const [isShowSendIcon, setIsShowSendIcon] = useState(false)
+
   const handleSubmit = (e) => {
     e.preventDefault()
     alert('Los datos han sido enviados.')
+  }
+
+  const handleHoverSend = (e) => {
+    e.type == 'mouseenter'
+    ? setIsShowSendIcon(true)
+    : setIsShowSendIcon(false) 
   }
 
   return (
@@ -17,25 +28,40 @@ export function ContactForm() {
         type='text'
         name='name'
         placeholder='Nombre / Empresa'
+        required
         className={styles['form__input']}
       />
       <input
         type='text'
         name='email'
         placeholder='Correo'
+        required
         className={styles['form__input']}
       />
       <textarea
         name='message'
         placeholder='Mensaje'
+        maxLength={100}
+        required
         className={`${styles['form__input']} ${styles['form__textarea']}`}
       ></textarea>
 
-      <input
+      <button
         type='submit'
-        value='Enviar'
-        className={`${styles['form__input']} ${styles['form__send']}`}
-      />
+        onMouseEnter={handleHoverSend}
+        onMouseLeave={handleHoverSend}
+        className={`${styles['form__input']} ${styles['form__send-button']}`}
+      >
+        {
+          isShowSendIcon
+          ? <FontAwesomeIcon icon={faPaperPlane} bounce />
+          : 'Enviar'
+        }
+      </button>
+
     </form>
   )
 }
+
+//Enviar
+//<FontAwesomeIcon icon={faPaperPlane} bounce />
